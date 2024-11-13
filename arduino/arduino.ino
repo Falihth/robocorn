3bb3789da3e2c85bb16397932de05aeaacade4c2
falih taufiqul hakim <falihtaufiqul@gmail.com>
2024-11-13 21:43:51 +0700

    add feat servo, buzzer, lcd,  and case 5 and 6

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,7 +1,19 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+const int buzzerPin = 7;
+LiquidCrystal_I2C lcd(0x27, 16, 2); // Alamat I2C 0x27, LCD 16x2
 
 void setup() {
   Serial.begin(9600);
   setupMotorPins();
+  setupServo();
+  pinMode(buzzerPin, OUTPUT);
+  
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("ROBOCORN");
 }
 
 void loop() {
@@ -12,25 +24,53 @@ void loop() {
     if (command > 0) {
       switch (command) {
         case 1:
+          beep();
           Serial.println("Maju");
+          lcd.setCursor(0, 1);
+          lcd.print("Maju     ");
           Motormaju();
-          delay(500);
           break;
 
         case 2:
           Serial.println("Kanan");
+          lcd.setCursor(0, 1);
+          lcd.print("Kanan    ");
           Motorkanan();
-          delay(500);
           break;
 
         case 3:
           Serial.println("Kiri");
+          lcd.setCursor(0, 1);
+          lcd.print("Kiri     ");
           Motorkiri();
-          delay(500);
+          break;
+
+        case 4:
+          Serial.println("Stop");
+          lcd.setCursor(0, 1);
+          lcd.print("Stop     ");
+          Motorstop();
+          break;
+
+        case 5:
+          Serial.println("Melubangi");
+          lcd.setCursor(0, 1);
+          lcd.print("Melubangi");
+          Lubang();
+          break;
+
+        case 6:
+          beep();
+          Serial.println("Bercocok");
+          lcd.setCursor(0, 1);
+          lcd.print("Bercocok ");
+          Benih();
           break;
 
         default:
           Serial.println("Perintah tidak dikenali");
+          lcd.setCursor(0, 1);
+          lcd.print("Tidak dikenali");
           Motorstop();
           break;
       }
@@ -40,4 +80,11 @@ void loop() {
       Serial.read();
     }
   }
+}
+
+void beep(){
+  digitalWrite(buzzerPin, HIGH);
+  delay(15);
+  digitalWrite(buzzerPin, LOW);
+  delay(15);
 }
